bf7c9407b3f4d547a2efd38f1f9356f57534ab51
Shanpei Chen <shanpeic@linux.alibaba.com>
2021-11-16 14:08:52 +0800

    cli: simplify cmdline parameters of cli tool
    cli.py init:
        used for for release mode, get required file from kernel
        src/devel/debuginfo rpm.
    
    cli.py dev_init:
        used for develep mode. Developer can test their locally build
        kernel. All required file are gained from user-defined kernel
        build directory.
    
    Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>
    Reviewed-by: Yihao Wu <wuyihao@linux.alibaba.com>

--- cli.py
+++ cli.py
@@ -10,6 +10,7 @@ except ImportError:
 import sh
 from sh import rsync, cp, glob as _glob
 from sched_boundary import check_sym_duplicy
+from multiprocessing import cpu_count
 import coloredlogs
 import logging
 import uuid
@@ -29,20 +30,16 @@ coloredlogs.install(level='INFO')
 logging.getLogger().addHandler(ShutdownHandler())
 
 class Plugsched(object):
-    def __init__(self, kernel_path, mod_path, threads, kernel_debuginfo_path='', kernel_devel_path=''):
+    def __init__(self, mod_path, vmlinux):
         self.plugsched_path = os.path.abspath(os.path.dirname(__file__))
-        self.kernel_path = kernel_path
         self.mod_path = mod_path
-        self.kernel_debuginfo_path = kernel_debuginfo_path
-        self.kernel_devel_path = kernel_devel_path
-        self.search_springboard = sh.Command(os.path.join(self.plugsched_path, 'springboard_search.sh')) \
-                                    .bake(_cwd=self.kernel_debuginfo_path)
+        self.vmlinux = vmlinux
+        self.search_springboard = sh.Command(os.path.join(self.plugsched_path, 'springboard_search.sh'))
 
         plugsched_sh = sh(_cwd=self.plugsched_path)
-        kernel_sh = sh(_cwd=self.kernel_path)
         mod_sh = sh(_cwd=self.mod_path)
 
-        self.plugsched_sh, self.kernel_sh, self.mod_sh = plugsched_sh, kernel_sh, mod_sh
+        self.plugsched_sh, self.mod_sh = plugsched_sh, mod_sh
 
         with open(os.path.join(self.plugsched_path, 'sched_boundary/sched_boundary.yaml')) as f:
             self.config = load(f, Loader)
@@ -55,7 +52,7 @@ class Plugsched(object):
             'src/plugsched.lds': 'kernel/sched/mod/',
             'src/Makefile.plugsched': './'
         }
-        self.threads = threads
+        self.threads = cpu_count()
         self.mod_files = self.config['mod_files']
         self.mod_srcs = [f for f in self.mod_files if f.endswith('.c')]
         self.mod_hdrs = [f for f in self.mod_files if f.endswith('.h')]
@@ -86,37 +83,36 @@ class Plugsched(object):
                         self.extracted_mod_srcs,
                         in_place=True)
 
-    def extract(self, system_map):
+    def extract(self):
         logging.info('Extracting scheduler module objs: %s', ' '.join(self.mod_objs))
         self.make(SCHED_MOD_STAGE = 'collect')
         self.make(SCHED_MOD_STAGE = 'analyze',
-                  SYSTEM_MAP      = system_map)
+                  SYSTEM_MAP      = './System.map')
         self.make(SCHED_MOD_STAGE = 'extract',
                   objs            = self.mod_objs)
         with open(os.path.join(self.mod_path, 'kernel/sched/mod/export_jump.h'), 'a') as f:
             sh.sort(glob('kernel/sched/*.fn_ptr.h', _cwd=self.mod_path), _out=f)
 
-    def create_mod(self, kernel_config):
+    def create_mod(self, kernel_src):
         logging.info('Creating mod build directory structure')
-        rsync(self.kernel_path + '/', self.mod_path + '/', archive=True, verbose=True, delete=True, exclude=".git")
+        rsync(kernel_src + '/', self.mod_path + '/', archive=True, verbose=True, delete=True, exclude='.git', filter=':- .gitignore')
         self.mod_sh.mkdir('kernel/sched/mod', parents=True)
 
         for f, t in self.file_mapping.items():
             self.mod_sh.cp(glob(f, _cwd=self.plugsched_path), t)
 
-        if kernel_config:
-            self.mod_sh.cp(kernel_config, '.config', force=True)
-        else:
-            arch = sh.grep(sh.lscpu(), 'Architecture:').split()[1]
-            self.mod_sh.cp('configs/config-4.19.y-' + arch, '.config', force=True)
+
+    def cmd_init(self, kernel_src, system_map, sym_vers, kernel_config, makefile):
+        self.create_mod(kernel_src)
+        self.plugsched_sh.cp(system_map,    self.mod_path, force=True)
+        self.plugsched_sh.cp(sym_vers,      self.mod_path, force=True)
+        self.plugsched_sh.cp(kernel_config, self.mod_path + '/.config', force=True)
+        self.plugsched_sh.cp(makefile,      self.mod_path, force=True)
+        self.plugsched_sh.cp(self.vmlinux,  self.mod_path, force=True)
 
         logging.info('Patching kernel kbuild system')
         self.apply_patch('kbuild.patch')
 
-    def cmd_init(self, system_map, kernel_config=''):
-        if not os.path.exists(kernel_config):
-            logging.fatal("Kernel config not specified")
-        self.create_mod(kernel_config)
         # precompile some files to avoid ugly building trouble
         self.mod_sh.make(
             'scripts/mod/',
@@ -136,7 +132,7 @@ class Plugsched(object):
             self.mod_sh.sed('/EXPORT_PLUGSCHED(release_task_reserve/d', 'kernel/sched/mod/export_jump.h', in_place=True)
             self.mod_sh.sed('/EXPORT_PLUGSCHED(init_task_reserve/d', 'kernel/sched/mod/export_jump.h', in_place=True)
 
-        self.extract(system_map)
+        self.extract()
         with open(os.path.join(self.mod_path, '.gitignore'), 'a') as f:
             f.write('*.sched_boundary\n*.fn_ptr.h')
         logging.info('Fixing up extracted scheduler module')
@@ -152,7 +148,7 @@ class Plugsched(object):
             self.apply_patch('dynamic_springboard.patch')
 
         try:
-            springboard = self.search_springboard('vmlinux')
+            springboard = self.search_springboard(self.vmlinux)
 
             if len(list(springboard)) != 1:
                 logging.error("Search springboard faild!")
@@ -176,12 +172,11 @@ class Plugsched(object):
         self.plugsched_sh.mkdir('rpmbuild')
         rpmbase_sh = sh(_cwd=rpmbuild_root)
         rpmbase_sh.mkdir(['BUILD','RPMS','SOURCES','SPECS','SRPMS'])
-        VERSION = self.kernel_sh.awk('-F=', '/^VERSION/{print $2}', 'Makefile').strip()
-        PATCHLEVEL = self.kernel_sh.awk('-F=', '/^PATCHLEVEL/{print $2}', 'Makefile').strip()
-        SUBLEVEL = self.kernel_sh.awk('-F=', '/^SUBLEVEL/{print $2}', 'Makefile').strip()
+        VERSION = self.mod_sh.awk('-F=', '/^VERSION/{print $2}', 'Makefile').strip()
+        PATCHLEVEL = self.mod_sh.awk('-F=', '/^PATCHLEVEL/{print $2}', 'Makefile').strip()
+        SUBLEVEL = self.mod_sh.awk('-F=', '/^SUBLEVEL/{print $2}', 'Makefile').strip()
         KVER = '%s.%s.%s' % (VERSION, PATCHLEVEL, SUBLEVEL)
         rpmname = 'plugsched-{}'.format(KVER)
-        self.mod_sh.cp(os.path.join(self.kernel_devel_path, 'Module.symvers'), '.')
 
         self.plugsched_sh.cp('plugsched.spec', os.path.join(rpmbuild_root, 'SPECS'), force=True)
         rpmbase_sh.rpmbuild('--define', '%%_outdir %s' % os.path.realpath(self.plugsched_path),
@@ -192,7 +187,6 @@ class Plugsched(object):
                             '--define', '%%name %s' % rpmname,
                             '--define', '%%threads %d' % self.threads,
                             '-bb', 'SPECS/plugsched.spec')
-        #check_sym_duplicy.run_test(self.mod_path, self.kernel_debuginfo_path)
         logging.info("Succeed!")
 
 class PlugschedCLI(object):
@@ -211,23 +205,62 @@ class PlugschedCLI(object):
             sh.yum.install('python-devel', 'gcc', 'gcc-plugin-devel', _fg=True)
         depsh.make(jobs=j)
 
-    def init(self, kernel_path, mod_path, kernel_debuginfo_path, j=1, kernel_config='',
-			system_map=''):
+    def init(self, release_kernel, kernel_src, mod_path):
         """ Initialize a scheduler module for a specific kernel release and product
 
-        :param j: Number of threads. "-j N" is okay while "-jN" is not allowed.
-        :param kernel_config: Specify kernel_config to create scheduler module
+        :param kernel_release: `uname -r` of target kernel to be hotpluged
+        :param kernel_src: kernel source code directory
+        :param mod_path: target working directory to develop new scheduler module
         """
-        self.plugsched = Plugsched(kernel_path, mod_path, threads=j, kernel_debuginfo_path=kernel_debuginfo_path)
-        self.plugsched.cmd_init(system_map, kernel_config)
 
-    def build(self, kernel_path, mod_path, kernel_devel_path, kernel_debuginfo_path, j=1):
+        vmlinux = '/usr/lib/debug/lib/modules/' + release_kernel + '/vmlinux'
+        if not os.path.exists(vmlinux):
+            logging.fatal("%s not found, please install kernel-debuginfo-%s.rpm", release_kernel)
+
+        system_map    = '/usr/src/kernels/' + release_kernel + '/System.map'
+        sym_vers      = '/usr/src/kernels/' + release_kernel + '/Module.symvers'
+        kernel_config = '/usr/src/kernels/' + release_kernel + '/.config'
+        makefile      = '/usr/src/kernels/' + release_kernel + '/Makefile'
+
+        if not os.path.exists(kernel_config):
+            logging.fatal("%s not found, please install kernel-devel-%s.rpm", release_kernel)
+
+        self.plugsched = Plugsched(mod_path, vmlinux)
+        self.plugsched.cmd_init(kernel_src, system_map, sym_vers, kernel_config, makefile)
+
+    def dev_init(self, kernel_src, mod_path):
+        """ Initialize plugsched development envrionment from kernel source code
+
+        :param kernel_src: kernel source code directory
+        :param mod_path: target working directory to develop new scheduler module
+        """
+
+        if not os.path.exists(kernel_src):
+            logging.fatal("Kernel source directory not exists")
+
+        vmlinux = os.path.join(kernel_src, 'vmlinux')
+        if not os.path.exists(vmlinux):
+            logging.fatal("%s not found, please execute `make -j %s` firstly", vmlinux, cpu_count())
+
+        system_map    = os.path.join(kernel_src, 'System.map')
+        sym_vers      = os.path.join(kernel_src, 'Module.symvers')
+        kernel_config = os.path.join(kernel_src, '.config')
+        makefile      = os.path.join(kernel_src, 'Makefile')
+
+        if not os.path.exists(kernel_config):
+            logging.fatal("kernel config %s not found", kernel_config)
+
+        self.plugsched = Plugsched(mod_path, vmlinux)
+        self.plugsched.cmd_init(kernel_src, system_map, sym_vers, kernel_config, makefile)
+
+    def build(self, mod_path):
         """ Build a scheduler module rpm package for a specific kernel release and product
 
-        :param product: The name of the product, eg. odps/fc/drds/ecs
-        :param j: Number of threads. "-j N" is okay while "-jN" is not allowed.
+        :param mod_path: target working directory to develop new scheduler module
         """
-        self.plugsched = Plugsched(kernel_path, mod_path, threads=j, kernel_debuginfo_path=kernel_debuginfo_path, kernel_devel_path=kernel_devel_path)
+
+        vmlinux = os.path.join(mod_path, 'vmlinux')
+        self.plugsched = Plugsched(mod_path, vmlinux)
         self.plugsched.cmd_build()
 
     def self_debug(self, func, *args, **kwargs):
