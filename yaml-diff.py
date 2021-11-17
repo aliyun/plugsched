cb687b6e24fd96fc2fb447e08201b7e3bb68d0ad
Shanpei Chen <shanpeic@linux.alibaba.com>
2021-11-17 09:48:34 +0800

    tools: add yaml-diff to print difference of two yaml files
    Until now, the input of yaml-diff can only be sched_boundary_extract.yaml,
    and only compare the [outsider] sections. In future, we can do more.
    
    Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>
    Reviewed-by: Erwei Deng <erwei@linux.alibaba.com>

---
+++ yaml-diff.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+
+from yaml import load, dump
+from yaml import CLoader as Loader, CDumper as Dumper
+import coloredlogs
+import logging
+import fire
+coloredlogs.install(level='INFO')
+
+def YamlDiff(old_file, new_file):
+
+    """ find the difference of two sched_boundary_extract.yaml
+
+    :param old_file: the 1st yaml file
+    :param new_file: the 2nd yaml file
+    """
+
+    with open(old_file) as f:
+        old_yaml = load(f, Loader)
+
+    with open(new_file) as f:
+        new_yaml = load(f, Loader)
+
+    old_set = set(old_yaml['function']['outsider'])
+    new_set = set(new_yaml['function']['outsider'])
+
+    for changed in (old_set | new_set) - (old_set & new_set):
+        logging.warn('DIFF: check the outsider \"%s\"', changed)
+
+    logging.info("Bye: analyze the DIFF and remember to update sched_boundary.yaml")
+
+if __name__ == '__main__':
+    fire.Fire(YamlDiff)
