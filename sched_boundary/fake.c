d11db8680f8be00da3ea795a50ff8c90f1aa5bf0
Yihao Wu <wuyihao@linux.alibaba.com>
2022-01-14 19:17:32 +0800

    border: Make fake.c work on older kernels
    kernel(<4.12) has no kernel/sched/sched-pelt.h
    kernel(<4.19) has no kernel/sched/pelt.h
    
    Only with this version comparation, can sched boundary extraction work
    on these older kernels.
    
    Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
    Reviewed-by: Shanpei Chen <shanpeic@linux.alibaba.com>

--- sched_boundary/fake.c
+++ sched_boundary/fake.c
@@ -1,5 +1,12 @@
 /* other headers not list below are included by sched.h */
 
+#include <linux/version.h>
 #include "kernel/sched/sched.h"
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
 #include "kernel/sched/sched-pelt.h"
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0)
 #include "kernel/sched/pelt.h"
+#endif
