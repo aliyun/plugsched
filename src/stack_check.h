aliyun/plugsched

commit 48a2d89080b03374705156a3fdd8eb8bf9dfe387

src: use stack_trace_save_tsk() to check stack if CONFIG_ARCH_STACKWALK is enabled
If CONFIG_ARCH_STACKWALK is enabled in v5.10, function save_stack_trace_tsk()
is deprecated, the struct definition of stack_trace is deprecated, too.
And we can use stack_trace_save_tsk() to replace it, which is similar to
save_stack_trace_tsk().

Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>

src/stack_check.h
@@ -3,6 +3,7 @@
 
 #include <linux/list.h>
 #include <trace/events/sched.h>
+#include <linux/stacktrace.h>
 #include "helper.h"
 
 #define MAX_STACK_ENTRIES	100
@@ -43,36 +44,27 @@ static void stack_check_init(void)
 	addr_sort(mod_func_addr, mod_func_size, NR_INTERFACE_FN);
 }
 
-static int stack_check_fn_insmod(struct stack_trace *trace)
+static int stack_check_fn(unsigned long *entries, unsigned int nr_entries, bool install)
 {
-	unsigned long address;
-	int i, idx;
+	int i, ret;
+	unsigned long *func_addr;
+	unsigned long *func_size;
 
-	for (i = 0; i < trace->nr_entries; i++) {
-		address = trace->entries[i];
-		idx = bsearch(vm_func_addr, 0, NR_INTERFACE_FN - 1, address);
-		if (idx == -1)
-			continue;
-
-		if (address < vm_func_addr[idx] + vm_func_size[idx])
-			return -EAGAIN;
+	if (install) {
+		func_addr = vm_func_addr;
+		func_size = vm_func_size;
+	} else {
+		func_addr = mod_func_addr;
+		func_size = mod_func_size;
 	}
 
-	return 0;
-}
+	for (i = 0; i < nr_entries; i++) {
+		int idx;
 
-static int stack_check_fn_rmmod(struct stack_trace *trace)
-{
-	unsigned long address;
-	int i, idx;
-
-	for (i = 0; i < trace->nr_entries; i++) {
-		address = trace->entries[i];
-		idx = bsearch(mod_func_addr, 0, NR_INTERFACE_FN - 1, address);
+		idx = bsearch(func_addr, 0, NR_INTERFACE_FN - 1, address);
 		if (idx == -1)
-			continue;
-
-		if (address < mod_func_addr[idx] + mod_func_size[idx])
+			return 0;
+		if (address < func_addr[idx] + func_size[idx])
 			return -EAGAIN;
 	}
 
@@ -83,6 +75,9 @@ static int stack_check_fn_rmmod(struct stack_trace *trace)
 static int stack_check_task(struct task_struct *task, bool install)
 {
 	unsigned long entries[MAX_STACK_ENTRIES];
+	unsigned int nr_entries = 0;
+
+#ifndef CONFIG_ARCH_STACKWALK
 	struct stack_trace trace;
 
 	trace.skip = 0;
@@ -91,11 +86,12 @@ static int stack_check_task(struct task_struct *task, bool install)
 	trace.entries = entries;
 
 	save_stack_trace_tsk(task, &trace);
+	nr_entries = trace.nr_entries;
+#else /* CONFIG_ARCH_STACKWALK */
+	nr_entries = stack_trace_save_tsk(task, &entries, MAX_STACK_ENTRIES, 0);
+#endif /* CONFIG_ARCH_STACKWALK */
 
-	if (install)
-		return stack_check_fn_insmod(&trace);
-	else
-		return stack_check_fn_rmmod(&trace);
+	return stack_check_fn(entries, nr_entries, install);
 }
 
 static int stack_check(bool install)

commit db4e0b1f6d97b53433a66cf58d58e09f578c3e6b

src: fix a compilation error of stack checking
Missing a local variable to get stack address.

Fixes: 48a2d890(src: use stack_trace_save_tsk() to check stack if
CONFIG_ARCH_STACKWALK is enabled)
Reported-by: Shanpei Chen <shanpeic@linux.alibaba.com>
Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>

src/stack_check.h
@@ -60,6 +60,7 @@ static int stack_check_fn(unsigned long *entries, unsigned int nr_entries, bool 
 
 	for (i = 0; i < nr_entries; i++) {
 		int idx;
+		unsigned long address = entries[i];
 
 		idx = bsearch(func_addr, 0, NR_INTERFACE_FN - 1, address);
 		if (idx == -1)