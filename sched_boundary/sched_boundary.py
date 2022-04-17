aliyun/plugsched

commit 481a3273e9272ef00ee75cff2561dd532abc2a7d

border: fixed Gcc bugs about typedef struct and va_list arguments
There are two bugs when rebuilding function decleration from gcc tree:

1. if argument's type is a typedef alias of a struct type, the 'struct'
keyword would missing when rebuilding.

2. if the argument type is va_list, the rebuilding result is 'struct
*', which is incorrect. The major cause is that the va_list is a
gcc builtin type, and its gcc tree orgnization is different from regular
types.

Signed-off-by: Qiao Ma <mqaio@linux.alibaba.com>

sched_boundary/sched_boundary.py
@@ -53,6 +53,14 @@ class GccBugs(object):
         return GccBugs.array_pointer_re.sub(r'\1 (*\3)[\2]', str)
 
     @staticmethod
+    def typedef(decl, str):
+        if isinstance(decl.type.name, gcc.TypeDecl):
+            name = decl.type.name.name
+            return str.replace('struct ' + name, name)
+        else:
+            return str
+
+    @staticmethod
     def enum_type_name(decl, str):
         if isinstance(decl.type, gcc.EnumeralType):
             i = str.find(decl.type.name.name)
@@ -61,14 +69,26 @@ class GccBugs(object):
             return str
 
     @staticmethod
+    def is_val_list(arg):
+        return isinstance(arg.type, gcc.PointerType) and \
+               isinstance(arg.type.dereference, gcc.RecordType) and \
+               isinstance(arg.type.dereference.name, gcc.Declaration) and \
+               arg.type.dereference.name.is_builtin and \
+               arg.type.dereference.name.name == '__va_list_tag'
+
+    @staticmethod
     def function_signature(decl):
         fn_signature = str(decl.str_decl)
         fn_signature = GccBugs.enum_type_name(decl.result, fn_signature)
-
         for arg in decl.arguments:
+            # special dealing with enum types. enum type will missing enum keyword in func.str_decl
             if isinstance(arg.type, gcc.EnumeralType):
                 arg_type_name = str(arg.type.name)
                 fn_signature = fn_signature.replace(arg_type_name, 'enum ' + arg_type_name)
+            # special dealing with builtin types, for example: va_list
+            elif GccBugs.is_val_list(arg):
+                fn_signature = fn_signature.replace("struct  *", "va_list")
+
         return fn_signature
 
     # extern type array[<unknown>] -> extern type array[]
@@ -79,7 +99,7 @@ class GccBugs(object):
     @staticmethod
     def fix(decl, str):
         for bugfix in [GccBugs.array_pointer, GccBugs.enum_type_name,
-                       GccBugs.array_size]:
+                       GccBugs.array_size, GccBugs.typedef]:
             str = bugfix(decl, str)
         return str
 

commit d30b6383beb0d7e585eaf15ec71b9289a6f2efca

border: fixed the extraction bug about typedef struct
The gcc.PointerType needs to be dereferenced.

Fixes: 481a3273(border: fixed Gcc bugs about typedef struct and va_list
arguments)
Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>

sched_boundary/sched_boundary.py
@@ -54,8 +54,13 @@ class GccBugs(object):
 
     @staticmethod
     def typedef(decl, str):
-        if isinstance(decl.type.name, gcc.TypeDecl):
-            name = decl.type.name.name
+        t = decl.type
+
+        while isinstance(t, (gcc.PointerType, gcc.ArrayType)):
+            t = t.dereference
+
+        if isinstance(t.name, gcc.TypeDecl):
+            name = t.name.name
             return str.replace('struct ' + name, name)
         else:
             return str