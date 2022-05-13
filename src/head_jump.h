aliyun/plugsched

commit 2df9e3475e8e9c0370fe32c4367fdb947b89da2d

src: simplify vmlinux symbol searching
We now use __orig_ prefix to distinguish symbols with the same name
between the module and vmlinux. So we can directly declare
extern __orig_func(params); before using them. And we don't need to use
kallsyms_lookup_name anymore.

There were some prefixes like orig_, which do the same as __vmlinux__.
But we abandon "orig_" and adopt "__orig_", because there are already
some orig_xxx symbols in vmlinux.

Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>

src/head_jump.h
@@ -32,8 +32,8 @@ static unsigned long mod_func_size[NR_INTERFACE_FN];
 #undef PLUGSCHED_FN_PTR
 
 /* Used to declare extern functions defined in vmlinux*/
-#define PLUGSCHED_FN_PTR(fn, ret, ...) extern ret __vmlinux__##fn(__VA_ARGS__);
-#define EXPORT_PLUGSCHED(fn, ret, ...) extern ret __vmlinux__##fn(__VA_ARGS__);
+#define PLUGSCHED_FN_PTR(fn, ret, ...) extern ret __orig_##fn(__VA_ARGS__);
+#define EXPORT_PLUGSCHED(fn, ret, ...) extern ret __orig_##fn(__VA_ARGS__);
 #include "export_jump.h"
 #undef EXPORT_PLUGSCHED
 #undef PLUGSCHED_FN_PTR
@@ -64,18 +64,16 @@ static unsigned long mod_func_size[NR_INTERFACE_FN];
 #define DEFINE_JUMP_FUNC(func) 	\
 	static unsigned char store_jump_##func[HEAD_LEN]; 	\
 	static unsigned char store_orig_##func[HEAD_LEN]; 	\
-	static unsigned long orig_##func; 			\
 	static unsigned long orig_##func##_size; 		\
 	static unsigned long mod_##func##_size
 
 #define JUMP_INIT_FUNC(func, prefix) do {		\
-		orig_##func = (unsigned long)__vmlinux__##func; 	\
-		vm_func_addr[NR_##func] = orig_##func; 		\
+		vm_func_addr[NR_##func] = (unsigned long)__orig_##func; 	\
 		mod_func_addr[NR_##func] = (unsigned long)prefix##func; \
-		memcpy(store_orig_##func, (unsigned char *)orig_##func, HEAD_LEN); \
+		memcpy(store_orig_##func, __orig_##func, HEAD_LEN); \
 		store_jump_##func[0] = 0xe9; 	\
 		(*(int *)(store_jump_##func + 1)) = 	\
-			(long)prefix##func - (long)orig_##func - HEAD_LEN; \
+			(long)prefix##func - (long)__orig_##func - HEAD_LEN; \
 	} while(0)
 
 #define JUMP_INSTALL_FUNC(func) \
@@ -103,24 +101,22 @@ static inline void do_write_cr0(unsigned long val)
 #define DEFINE_JUMP_FUNC(func)				\
 	static u32 store_orig_##func;			\
 	static u32 store_jump_##func;			\
-	static unsigned long orig_##func;		\
 	static unsigned long orig_##func##_size;	\
 	static unsigned long mod_##func##_size
 
 #define JUMP_INIT_FUNC(func, prefix) do {	\
-		orig_##func = (unsigned long)__vmlinux__##func;	\
-		vm_func_addr[NR_##func] = orig_##func; 		\
+		vm_func_addr[NR_##func] = (unsigned long)__orig_##func; 	\
 		mod_func_addr[NR_##func] = (unsigned long)prefix##func; \
-		memcpy((void *)&store_orig_##func, (void *)orig_##func, AARCH64_INSN_SIZE); \
-		store_jump_##func = aarch64_insn_gen_branch_imm(orig_##func,	\
+		memcpy((void *)&store_orig_##func, __orig_##func, AARCH64_INSN_SIZE); \
+		store_jump_##func = aarch64_insn_gen_branch_imm((unsigned long)__orig_##func,	\
 				  (unsigned long)prefix##func, AARCH64_INSN_BRANCH_NOLINK); \
 	} while(0)
 
 #define JUMP_INSTALL_FUNC(func) \
-	aarch64_insn_patch_text_nosync((void *)orig_##func, store_jump_##func)
+	aarch64_insn_patch_text_nosync(__orig_##func, store_jump_##func)
 
 #define JUMP_REMOVE_FUNC(func)  \
-	aarch64_insn_patch_text_nosync((void *)orig_##func, store_orig_##func)
+	aarch64_insn_patch_text_nosync(__orig_##func, store_orig_##func)
 
 #define JUMP_OPERATION(ops) do {	\
 		jump_##ops();	\

src/main.c
@@ -18,6 +18,7 @@
 
 #define MAX_CPU_NR		1024
 
+extern void __orig___schedule(bool);
 int process_id[MAX_CPU_NR];
 atomic_t cpu_finished;
 static atomic_t global_error;
@@ -42,7 +43,6 @@ static s64 stop_time;
 ktime_t stop_time_p0, stop_time_p1, stop_time_p2;
 ktime_t main_start, main_end, init_start, init_end;
 
-extern void init_sched_rebuild(void);
 extern void clear_sched_state(bool mod);
 extern void rebuild_sched_state(bool mod);
 
@@ -119,7 +119,7 @@ static void reset_balance_callback(void)
 static void disable_stack_protector(void)
 {
 	int i;
-	void *addr = orig___schedule + STACK_PROTECTOR;
+	void *addr = __orig___schedule + STACK_PROTECTOR;
 
 	for (i=0; i<STACK_PROTECTOR_LEN; i++, addr+=4)
 		aarch64_insn_patch_text_nosync(addr, NOP);
@@ -230,15 +230,15 @@ static int sync_sched_mod(void *func)
 }
 
 #ifdef CONFIG_SCHED_DEBUG
+extern void __orig_register_sched_domain_sysctl(void);
+extern void __orig_unregister_sched_domain_sysctl(void);
+
 static inline void install_sched_domain_sysctl(void)
 {
-	void (*old_unregister_sd_sysctl)(void);
-
 	mutex_lock(&cgroup_mutex);
 	mutex_lock(&cpuset_mutex);
 
-	old_unregister_sd_sysctl = (void *)kallsyms_lookup_name("unregister_sched_domain_sysctl");
-	old_unregister_sd_sysctl();
+	__orig_unregister_sched_domain_sysctl();
 	register_sched_domain_sysctl();
 
 	mutex_unlock(&cpuset_mutex);
@@ -247,15 +247,12 @@ static inline void install_sched_domain_sysctl(void)
 
 static inline void restore_sched_domain_sysctl(void)
 {
-	void (*old_register_sd_sysctl)(void);
-
 	mutex_lock(&cgroup_mutex);
 	mutex_lock(&cpuset_mutex);
 
 	unregister_sched_domain_sysctl();
 	cpumask_copy(sd_sysctl_cpus, cpu_possible_mask);
-	old_register_sd_sysctl = (void *)kallsyms_lookup_name("register_sched_domain_sysctl");
-	old_register_sd_sysctl();
+	__orig_register_sched_domain_sysctl();
 
 	mutex_unlock(&cpuset_mutex);
 	mutex_unlock(&cgroup_mutex);
@@ -279,13 +276,13 @@ void install_sched_debugfs(void)
 				&sched_feat_fops);
 }
 
+extern struct file_operations __orig_sched_feat_fops;
+extern struct seq_operations  __orig_sched_debug_sops;
+
 void restore_sched_debugfs(void)
 {
-	struct file_operations *old_schedfeat_fops =
-		(struct file_operations *)kallsyms_lookup_name("sched_feat_fops");
-
 	debugfs_remove(sched_features_dir);
-	debugfs_create_file("sched_features", 0644, NULL, NULL, old_schedfeat_fops);
+	debugfs_create_file("sched_features", 0644, NULL, NULL, &__orig_sched_feat_fops);
 }
 
 /* sched_debug interface in proc */
@@ -301,12 +298,9 @@ int install_sched_debug_procfs(void)
 
 int restore_sched_debug_procfs(void)
 {
-	struct seq_operations* old_sched_debug_sops =
-		(struct seq_operations *)kallsyms_lookup_name("sched_debug_sops");
-
 	remove_proc_entry("sched_debug", NULL);
 
-	if (!proc_create_seq("sched_debug", 0444, NULL, old_sched_debug_sops))
+	if (!proc_create_seq("sched_debug", 0444, NULL, &__orig_sched_debug_sops))
 		return -ENOMEM;
 
 	return 0;
@@ -314,6 +308,8 @@ int restore_sched_debug_procfs(void)
 #endif
 
 #ifdef CONFIG_SCHEDSTATS
+extern struct seq_operations __orig_schedstat_sops;
+
 /* schedstat interface in proc */
 int install_proc_schedstat(void)
 {
@@ -327,12 +323,9 @@ int install_proc_schedstat(void)
 
 int restore_proc_schedstat(void)
 {
-	struct seq_operations* old_schedstat_sops =
-		(struct seq_operations*)kallsyms_lookup_name("schedstat_sops");
-
 	remove_proc_entry("schedstat", NULL);
 
-	if (!proc_create_seq("schedstat", 0444, NULL, old_schedstat_sops))
+	if (!proc_create_seq("schedstat", 0444, NULL, &__orig_schedstat_sops))
 		return -ENOMEM;
 
 	return 0;
@@ -558,9 +551,8 @@ static int __init sched_mod_init(void)
 	printk("Hi, scheduler mod is installing!\n");
 	init_start = ktime_get();
 
-	sched_springboard = kallsyms_lookup_name("__schedule") + SPRINGBOARD;
+	sched_springboard = (unsigned long)__orig___schedule + SPRINGBOARD;
 
-	init_sched_rebuild();
 	jump_init_all();
 
 	/* This must after jump_init_all function !!! */

src/sched_rebuild.c
@@ -6,18 +6,10 @@
 #include <linux/sched.h>
 #include "sched.h"
 
-static void (*orig_set_rq_offline)(struct rq *);
-static void (*orig_set_rq_online)(struct rq *);
+extern void __orig_set_rq_offline(struct rq*);
+extern void __orig_set_rq_online(struct rq*);
 
 extern unsigned int process_id[];
-
-void init_sched_rebuild(void)
-{
-	orig_set_rq_online = (void (*) (struct rq *))
-			kallsyms_lookup_name("set_rq_online");
-	orig_set_rq_offline = (void (*) (struct rq *))
-			kallsyms_lookup_name("set_rq_offline");
-}
 
 void clear_sched_state(bool mod)
 {
@@ -29,7 +21,7 @@ void clear_sched_state(bool mod)
 	if (mod) {
 		set_rq_offline(rq);
 	} else {
-		orig_set_rq_offline(rq);
+		__orig_set_rq_offline(rq);
 	}
 
 	for_each_process_thread(g, p) {
@@ -60,7 +52,7 @@ void rebuild_sched_state(bool mod)
 	if (mod) {
 		set_rq_online(rq);
 	} else {
-		orig_set_rq_online(rq);
+		__orig_set_rq_online(rq);
 	}
 
 	for_each_process_thread(g, p) {

src/stack_check.h
@@ -17,7 +17,7 @@ static void stack_check_init(void)
 {
 	#define PLUGSCHED_FN_PTR EXPORT_PLUGSCHED
 	#define EXPORT_PLUGSCHED(fn, ...) 				\
-		kallsyms_lookup_size_offset(orig_##fn, 			\
+		kallsyms_lookup_size_offset((unsigned long)__orig_##fn,			 \
 				&orig_##fn##_size, NULL); 		\
 		vm_func_size[NR_##fn] = orig_##fn##_size;
 

tools/symbol_resolve/symbol_resolve.cpp
@@ -81,11 +81,11 @@ static void resolve_ref(const char *fname, kallsym_collection &kallsyms, sympos_
 		if (sym.st_shndx != SHN_UNDEF)
 			continue;
 		/*
-		 * Filter out the "__vmlinux__" prefix, which represents interface
+		 * Filter out the "__orig_" prefix, which represents interface
 		 * functions or function pointers defined in vmlinux.
 		 */
-		if (strstr(name, "__vmlinux__"))
-			name += sizeof("__vmlinux__") - 1;
+		if (strstr(name, "__orig_"))
+			name += sizeof("__orig_") - 1;
 		if (kallsyms.find(name) == kallsyms.end())
 			continue;
 		kallsym = &kallsyms[name];

commit 6c032abf9e7be1237922c901a1e271b977fc0a3b

src: fix trivial mispell bug in head_jump
2df9e3475e8e tries to simplify symbol searhing on both aarch64 and x86_64.
But it didn't do it clean on x86_64 and breaks it.

Fixes: 2df9e3475e8e ("src: simplify vmlinux symbol searching")
Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>

src/head_jump.h
@@ -77,10 +77,10 @@ static unsigned long mod_func_size[NR_INTERFACE_FN];
 	} while(0)
 
 #define JUMP_INSTALL_FUNC(func) \
-	memcpy((unsigned char *)orig_##func, store_jump_##func, HEAD_LEN)
+	memcpy((unsigned char *)__orig_##func, store_jump_##func, HEAD_LEN)
 
 #define JUMP_REMOVE_FUNC(func) 	\
-	memcpy((unsigned char *)orig_##func, store_orig_##func, HEAD_LEN)
+	memcpy((unsigned char *)__orig_##func, store_orig_##func, HEAD_LEN)
 
 static inline void do_write_cr0(unsigned long val)
 {