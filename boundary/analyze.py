8dd010cce593e15a2da995a5971bfc5410590591
Shanpei Chen <shanpeic@linux.alibaba.com>
2022-07-26 21:22:24 +0800

    border: fix tainted and undefined function set
    In new sidecar implementation, there is no different between sidecar
    functions and interface functions. Both of them should belong to
    tainted and undefined function set (to calculate sympos).
    
    Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

--- boundary/analyze.py
+++ boundary/analyze.py
@@ -243,8 +243,8 @@ if __name__ == '__main__':
     func_class['sched_outsider'] = (func_class['mod_fns'] - func_class['insider'] - func_class['border']) | func_class['fn_ptr_optimized']
     func_class['optimized_out'] = func_class['sched_outsider'] - func_class['in_vmlinux']
     func_class['public_user'] = func_class['fn'] - func_class['insider'] - func_class['border']
-    func_class['tainted'] = (func_class['border'] | func_class['insider']) & func_class['in_vmlinux']
-    func_class['undefined'] = func_class['sched_outsider'] | func_class['border']
+    func_class['tainted'] = (func_class['border'] | func_class['insider'] | func_class['sidecar']) & func_class['in_vmlinux']
+    func_class['undefined'] = func_class['sched_outsider'] | func_class['border'] | func_class['sidecar']
 
     for output_item in ['sched_outsider', 'fn_ptr', 'interface', 'init', 'insider', 'optimized_out', 'export', 'sdcr_out']:
         config['function'][output_item] = func_class[output_item]
