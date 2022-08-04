aliyun/plugsched

commit 17f974edb839fb78e691376f7a021143474d8fcc

src: migrate sched_class for every task during upgrade and rollback
Only migrating sched_class for every task, we might think of sched_class
as a private data structure. And then we can change it whatever we want,
which help to imporve expressiveness of scheduler module.

Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

src/sched_rebuild.c
@@ -8,8 +8,48 @@
 
 extern void __orig_set_rq_offline(struct rq*);
 extern void __orig_set_rq_online(struct rq*);
-
 extern unsigned int process_id[];
+
+extern const struct sched_class __orig_stop_sched_class;
+extern const struct sched_class __orig_dl_sched_class;
+extern const struct sched_class __orig_rt_sched_class;
+extern const struct sched_class __orig_fair_sched_class;
+extern const struct sched_class __orig_idle_sched_class;
+
+static struct sched_class* class[][7] = {
+	{
+		&fair_sched_class,
+		&rt_sched_class,
+		&rt_sched_class,
+		&fair_sched_class,
+		NULL,
+		&idle_sched_class,
+		&dl_sched_class,
+		&stop_sched_class,
+	},
+	{
+		&__orig_fair_sched_class,
+		&__orig_rt_sched_class,
+		&__orig_rt_sched_class,
+		&__orig_fair_sched_class,
+		NULL,
+		&__orig_idle_sched_class,
+		&__orig_dl_sched_class,
+		&__orig_stop_sched_class,
+	}
+};
+
+static void switch_sched_class(struct task_struct *p, int mod)
+{
+	int policy = p->policy;
+	int idx = mod ? 0 : 1;
+
+	if (p == (task_rq(p)->stop))
+		policy = 7;
+
+	p->sched_class = class[idx][policy];
+	return;
+}
 
 void clear_sched_state(bool mod)
 {
@@ -58,6 +98,8 @@ void rebuild_sched_state(bool mod)
 	for_each_process_thread(g, p) {
 		if (rq != task_rq(p))
 			continue;
+
+		switch_sched_class(p, mod);
 
 		if (p == rq->stop)
 			continue;

commit 3eb0db935817804eb3fbdf42e1399c0a7ac29ba8

src: fix kernel panic when loading scheduler.ko
The invalid array size causes sched_class to be incorrectly initialized,
then lead to kernel panic.

Fixes: 17f974e ("src: migrate sched_class for every task during
       upgrade and rollback")

Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

src/sched_rebuild.c
@@ -16,7 +16,7 @@ extern const struct sched_class __orig_rt_sched_class;
 extern const struct sched_class __orig_fair_sched_class;
 extern const struct sched_class __orig_idle_sched_class;
 
-static struct sched_class* class[][7] = {
+static const struct sched_class* class[][8] = {
 	{
 		&fair_sched_class,
 		&rt_sched_class,