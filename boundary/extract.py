12968a6724ce4652c1053aca7815fe02669091fa
Shanpei Chen <shanpeic@linux.alibaba.com>
2022-08-05 16:06:29 +0800

    border: introduce PEP8 code style for extract.py
    Here are some major code style changes:
    1. yapf -i border/analyze.py
    2. write short comments for functions and classes;
    3. limit all lines to a maximum of 79 characters;
    3. delete unused import;
    5. change variable name (tmpdir -> tmp_dir, modpath -> mod_dir);
    6. move fix include path code to independent function;
    7. simplify the handling of per_CPU and static_key;
    
    PEP8 URL:  https://peps.python.org/pep-0008/
    yapf tool: sudo pip install yapf
    
    Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

--- boundary/extract.py
+++ boundary/extract.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python3
 # Copyright 2019-2022 Alibaba Group Holding Limited.
 # SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+"""Extract module code according to boundary information"""
 
-from collections import defaultdict
-from itertools import groupby as _groupby
-from yaml import load, dump, resolver, CLoader as Loader, CDumper as Dumper
 import json
 import re
 import os
 import sys
+from yaml import load, resolver, CLoader as Loader
 
 # Use set as the default sequencer for yaml
-Loader.add_constructor(resolver.BaseResolver.DEFAULT_SEQUENCE_TAG,
-                     lambda loader, node: set(loader.construct_sequence(node)))
-Dumper.add_representer(set, lambda dumper, node: dumper.represent_list(node))
+Loader.add_constructor(
+    resolver.BaseResolver.DEFAULT_SEQUENCE_TAG,
+    lambda loader, node: set(loader.construct_sequence(node)))
 
-# tmp directory to store middle files
-tmpdir = None
-
-# directory to store schedule module source code
-modpath = None
 
 class Extraction(object):
-    def __init__(self, src_file, tmpdir, modpath):
-        with open(tmpdir + 'boundary_extract.yaml') as f:
+
+    def __init__(self, src_file, tmp_dir, mod_dir):
+        with open(tmp_dir + 'boundary_extract.yaml') as f:
             self.config = load(f, Loader)
 
         self.src_file = src_file
-        self.modpath = modpath
+        self.mod_dir = mod_dir
         self.mod_files = self.config['mod_files']
         self.mod_srcs = {f for f in self.mod_files if f.endswith('.c')}
         self.mod_hdrs = self.mod_files - self.mod_srcs
@@ -38,7 +33,7 @@ class Extraction(object):
         self.var_list = []
 
         if src_file in self.mod_hdrs:
-            file_name = tmpdir + 'header_symbol.json'
+            file_name = tmp_dir + 'header_symbol.json'
         else:
             file_name = src_file + '.boundary'
 
@@ -48,25 +43,29 @@ class Extraction(object):
             self.meta_var = metas['var']
 
     def function_location(self):
+        """Get the source code location of border functions"""
         unique = set()
         for fn in self.meta_fn:
             # filter out *.h in *.c
-            if fn['file'] != self.src_file: continue
+            if fn['file'] != self.src_file:
+                continue
 
             # remove duplicated function
             obj = tuple(fn['signature'])
-            if obj in unique: continue
+            if obj in unique:
+                continue
             unique.add(obj)
 
-            if obj in self.config['function']['sched_outsider'] or \
-               obj in self.config['function']['sdcr_out']:
-                self.fn_list.append(fn),
+            if (obj in self.config['function']['sched_outsider'] or
+                    obj in self.config['function']['sdcr_out']):
+                self.fn_list.append(fn)
             elif obj in self.config['function']['callback']:
                 self.callback_list.append(fn)
             elif obj in self.config['function']['interface']:
                 self.interface_list.append(fn)
 
     def var_location(self):
+        """Get the source code location of shared global variables"""
         # sidecar shares all global variables with vmlinux
         if self.src_file in self.sdcr_srcs:
             self.var_list = self.meta_var
@@ -78,103 +77,111 @@ class Extraction(object):
             if var['name'] in self.config['global_var']['force_private']:
                 continue
             # share public (non-static) variables by default
-            if var['public'] or var['name'] in self.config['global_var']['extra_public']:
+            if (var['public'] or
+                    var['name'] in self.config['global_var']['extra_public']):
                 self.var_list.append(var)
 
-    # merge multi decl lines and return the merged line number
     def merge_up_lines(self, lines, curr):
+        """Merge up multi-lines-function-declaration into one line"""
         terminator = re.compile(';|}|#|//|\*/|^\n$')
         merged = lines[curr].strip()
 
         while curr >= 1:
-            line = lines[curr-1]
+            line = lines[curr - 1]
             if terminator.search(line):
-                break;
+                break
             merged = line.strip() + ' ' + merged
             lines[curr] = ''
             curr -= 1
 
         lines[curr] = merged.replace(' ;', ';') + '\n'
-        return curr;
+        return curr
 
     def function_extract(self, lines):
+        """Generate function code for new module"""
+        warn = "/* DON'T MODIFY FUNCTION {}, IT'S NOT PART OF SCHEDMOD */\n"
+        cb_warn = "/* DON'T MODIFY SIGNATURE OF CALLBACK FUNCTION {} */\n"
+        if_warn = "/* DON'T MODIFY SIGNATURE OF INTERFACE FUNCTION {} */\n"
+        decl_fmt = "extern {ret} {fn}({params});\n"
+
         for fn in self.fn_list:
             name, inline = fn['name'], fn['inline']
-            (row_start,col_start), (row_end,_) = fn['l_brace_loc'], fn['r_brace_loc']
+            (row_end, _) = fn['r_brace_loc']
+            (row_start, col_start) = fn['l_brace_loc']
 
-            if inline or \
-                    tuple(fn['signature']) in self.config['function']['optimized_out']:
-                lines[row_end] += "/* DON'T MODIFY FUNCTION {}, IT'S NOT PART OF SCHEDMOD */\n".format(name)
+            if (inline or tuple(fn['signature'])
+                    in self.config['function']['optimized_out']):
+                lines[row_end] += warn.format(name)
             else:
                 # convert function body "{}" to ";"
                 # only handle normal kernel function definition
                 lines[row_start] = lines[row_start][:col_start] + ";\n"
                 self.merge_up_lines(lines, row_start)
-                for i in range(row_start+1, row_end+1):
+                for i in range(row_start + 1, row_end + 1):
                     lines[i] = ''
 
-        callback_export_c_fmt = "extern {ret} {fn}({params});\n"
         for fn in self.callback_list:
             name, decl_str = fn['name'], fn['decl_str']
-            (row_start,col_start), (row_end,_) = fn['name_loc'], fn['r_brace_loc']
-
+            (row_start, _) = fn['name_loc']
+            (row_end, _) = fn['r_brace_loc']
             new_name = '__mod_' + name
-            lines[row_start] = lines[row_start][:col_start] + \
-                lines[row_start][col_start:].replace(name, '__used ' + new_name)
-            lines[row_end] = lines[row_end] + '\n' + \
-                "/* DON'T MODIFY SIGNATURE OF FUNCTION {}, IT'S CALLBACK FUNCTION */\n".format(new_name) + \
-                callback_export_c_fmt.format(**decl_str)
+            used_name = '__used ' + new_name
+
+            lines[row_start] = lines[row_start].replace(name, used_name)
+            lines[row_end] += ('\n' + cb_warn.format(new_name) +
+                               decl_fmt.format(**decl_str))
 
         for fn in self.interface_list:
-            name, decl_str, (row_end,_) = fn['name'], fn['decl_str'], fn['r_brace_loc']
+            name, (row_end, _) = fn['name'], fn['r_brace_loc']
 
             # everyone know that syscall ABI should be consistent
-            if any(name.startswith(prefix) for prefix in self.config['interface_prefix']):
+            if any(name.startswith(prefix)
+                   for prefix in self.config['interface_prefix']):
                 continue
-            lines[row_end] += \
-                "/* DON'T MODIFY SIGNATURE OF FUNCTION {}, IT'S INTERFACE FUNCTION */\n".format(name)
+            lines[row_end] += if_warn.format(name)
 
     def merge_down_lines(self, lines, curr):
-            merged = ''
-            start = curr
+        """Merge down multi-lines-var-definition into one line"""
+        merged = ''
+        start = curr
 
-            while curr < len(lines) and ';' not in lines[curr]:
-                merged += lines[curr].strip() + ' '
-                lines[curr] = ''
-                curr += 1
-
-            merged += lines[curr]
+        while curr < len(lines) and ';' not in lines[curr]:
+            merged += lines[curr].strip() + ' '
             lines[curr] = ''
-            lines[start] = merged
-            return curr
+            curr += 1
+
+        merged += lines[curr]
+        lines[curr] = ''
+        lines[start] = merged
+        return curr
 
     def var_extract(self, lines):
+        """Generate data declarition code for new module"""
         # General handling all shared variables
         for var in list(self.var_list):
-            name, row_start, (row_name,_) = var['name'], var['decl_start_line'], var['name_loc']
+            name, row_start = var['name'], var['decl_start_line']
+            (row_name, _) = var['name_loc']
 
             # Fixed variable name not on first line, e.g. nohz
-            for i in range(row_start+1, row_name):
+            for i in range(row_start + 1, row_name):
                 lines[i] = ''
 
-            # merge multi-lines-var-definition into one line
             self.merge_down_lines(lines, row_start)
 
-            # Specially handling shared per_cpu and static_key variables to improve readability
+            # Specially handling shared per_cpu and static_key variables
+            # to improve readability
             line = lines[row_start]
-            if 'DEFINE_PER_CPU(' in line:
-                line = line.replace('DEFINE_PER_CPU(', 'DECLARE_PER_CPU(').replace('static ', '')
-            elif 'DEFINE_PER_CPU_SHARED_ALIGNED(' in line:
-                line = line.replace('DEFINE_PER_CPU_SHARED_ALIGNED(', 'DECLARE_PER_CPU_SHARED_ALIGNED(').replace('static ', '')
-            elif 'DEFINE_STATIC_KEY_FALSE(' in line:
-                line = line.replace('DEFINE_STATIC_KEY_FALSE(', 'DECLARE_STATIC_KEY_FALSE(').replace('static ', '')
-            elif 'DEFINE_STATIC_KEY_TRUE(' in line:
-                line = line.replace('DEFINE_STATIC_KEY_TRUE(', 'DECLARE_STATIC_KEY_TRUE(').replace('static ', '')
-            else:
-                continue
+            replace_list = [
+                ('DEFINE_PER_CPU', 'DECLARE_PER_CPU'),
+                ('DEFINE_STATIC_KEY', 'DECLARE_STATIC_KEY'),
+            ]
 
-            lines[row_start] = line
-            self.var_list.remove(var)
+            for (p, repl) in replace_list:
+                if p in line:
+                    line = line.replace(p, repl).replace('static ', '')
+                    lines[row_start] = line
+                    self.var_list.remove(var)
+                    break
 
         # delete data definition
         for var in self.var_list:
@@ -186,48 +193,60 @@ class Extraction(object):
             row_start = var['decl_start_line']
             lines[row_start] += var['decl_str'] + '\n'
 
-    # fix trival code adaption
+    def fix_include(self, line):
+        """Fix header file path, assume one include per line"""
+        old_header = line.split('"')[1]
+        rel_header = os.path.join(os.path.dirname(self.src_file), old_header)
+        rel_header = os.path.relpath(rel_header)
+
+        # module header file is already extracted to the right place
+        if rel_header in self.mod_files:
+            return line
+
+        new_header = os.path.relpath(rel_header, self.mod_dir)
+        return line.replace(old_header, new_header)
+
     def fix_up(self, lines):
-        delete_patt = re.compile('initcall|early_param|__init |__initdata |__setup')
+        """Post fix trival code adaption"""
+        delete = re.compile('initcall|early_param|__init |__initdata |__setup')
         replace_list = [('struct atomic_t', 'atomic_t')]
 
         for (i, line) in enumerate(lines):
-            # fixup header file path, assume there is one include per line
             if '#include "' in line:
-                old_header = line.split('"')[1]
-                rel_header = os.path.join(os.path.dirname(self.src_file), old_header)
-                rel_header = os.path.relpath(rel_header)
-                # module header file is extracted to the right place
-                if rel_header in self.mod_files: continue
-
-                lines[i] = line.replace(old_header, os.path.relpath(rel_header, self.modpath))
+                lines[i] = self.fix_include(line)
                 continue
 
-            if delete_patt.search(line):
+            if delete.search(line):
                 lines[i] = ''
                 continue
 
             for (p, repl) in replace_list:
                 if p in line:
                     lines[i] = line.replace(p, repl)
-                    break;
+                    break
 
     def extract_file(self):
-        src_f = self.src_file
+        """Generate module source code"""
         self.function_location()
         self.var_location()
 
-        with open(src_f) as in_f, open(self.modpath + os.path.basename(src_f), 'w') as out_f:
+        src_f = self.src_file
+        res_f = self.mod_dir + os.path.basename(src_f)
+
+        with open(src_f) as in_f, open(res_f, 'w') as out_f:
             lines = in_f.readlines()
             self.function_extract(lines)
             self.var_extract(lines)
             self.fix_up(lines)
             out_f.writelines(lines)
 
+
 if __name__ == '__main__':
 
     src_file = sys.argv[1]
-    tmpdir = sys.argv[2]
-    modpath = sys.argv[3]
-    extract = Extraction(src_file, tmpdir, modpath)
+    # tmp directory to store middle files
+    tmp_dir = sys.argv[2]
+    # directory to store schedule module source code
+    mod_dir = sys.argv[3]
+    extract = Extraction(src_file, tmp_dir, mod_dir)
     extract.extract_file()
