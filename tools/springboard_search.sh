aliyun/plugsched

commit 11d1a5437a2c6bebf2753bd6598a0e6107e10cf3

tools: fix some compilation errors for kernel 4.19 aarch64
There are some compilation errors when building the scheduler module
for arm64 kernel-4.19. See the first error log:

  /root/scheduler/kernel/sched/mod//main.c: In function
  'disable_stack_protector':
  /root/scheduler/kernel/sched/mod//main.c:137:35: error:
  'STACK_PROTECTOR' undeclared (first use in this function)
    137 |  void *addr = __orig___schedule + STACK_PROTECTOR;
        |                                   ^~~~~~~~~~~~~~~

  /root/scheduler/kernel/sched/mod//main.c:137:35: note: each
  undeclared identifier is reported only once for each function it
  appears in /root/scheduler/kernel/sched/mod//main.c:139:14: error:
  'STACK_PROTECTOR_LEN' undeclared (first use in this function)
    139 |  for (i=0; i<STACK_PROTECTOR_LEN; i++, addr+=4)
        |              ^~~~~~~~~~~~~~~~~~

For the CONFIG_STACKPROTECTOR_PER_TASK, it will be added to config files
automatically when gcc version >= 9.1.0. The gcc version of
plugsched-sdk is 10.2.1 but 8.5.1 to build the kernel of anolis 8.6.
So, the reason of this error is that the springboard_search.sh tool uses
the /usr/src/kernels/release-kernel/.config to determine whether the
CONFIG_STACKPROTECTOR_PER_TASK is set, but this config is added to
config file later when building the kernel in init stage, which
causes that the value of this config is "N" in the tool and "Y" in
main.c.

So the main.c should not use the CONFIG_STACKPROTECTOR_PER_TASK. We can
reuse the STACK_PROTECTOR macro generated by springboard_search tool in
main.c.

I found another bug when fixing the bug above. See the error log:

  /root/scheduler/kernel/sched/mod//main.c:137:50: error: expected
  expression before ';' token
    137 |  void *addr = __orig___schedule + STACK_PROTECTOR;
        |                                                  ^

The reason of this error is that, the STACK_PROTECTOR and
STACK_PROTECTOR_LEN have the invalid value. See the following in
Makefile:

tools/springboard_search.sh
@@ -86,7 +86,7 @@ function get_stack_check_off_AArch64()
 		start == 1 && $3 == "ldr" {print "ldr"; next}
 		start == 1 && $3 == "ldp" {print "ldp"; next}
 		start == 1 && $3 == "ret" {print "ret"; next}
-		start == 1 && $NF== "<__schedule+'$stack_chk_fail_off'>" {print "chk"; next}
+		start == 1 && $5 == "<__schedule+'$stack_chk_fail_off'>" {print "chk"; next}
 		start == 1 {print "any"}' <<< "$schedule_asm")
 
 

src/main.c
@@ -129,7 +129,7 @@ static void reset_balance_callback(void)
 }
 
 
-#if defined(CONFIG_ARM64) && defined(CONFIG_STACKPROTECTOR_PER_TASK)
+#if defined(CONFIG_ARM64) && defined(STACK_PROTECTOR)
 #define NOP 0xd503201f
 static void disable_stack_protector(void)
 {

commit c2790a78688f09500f2842e2578a086eb79adcc8

tools: fix stackprotector search for aarch64
We search stackproctors by matching "<__schedule+OFFSET>" in the assembly,
where __schedule+OFFSET points to __stack_chk_fail.

11d1a5437a2c changes stack protector matching pattern from the last column
in the assembly to the 5th column. However awk counts from 1, so it should
actually be the 6th column.

Fixes: 11d1a5437a2c ("tools: fix some compilation errors for kernel 4.19 aarch64")
Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>

tools/springboard_search.sh
@@ -86,7 +86,7 @@ function get_stack_check_off_AArch64()
 		start == 1 && $3 == "ldr" {print "ldr"; next}
 		start == 1 && $3 == "ldp" {print "ldp"; next}
 		start == 1 && $3 == "ret" {print "ret"; next}
-		start == 1 && $5 == "<__schedule+'$stack_chk_fail_off'>" {print "chk"; next}
+		start == 1 && $6 == "<__schedule+'$stack_chk_fail_off'>" {print "chk"; next}
 		start == 1 {print "any"}' <<< "$schedule_asm")
 
 