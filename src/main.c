aliyun/plugsched

commit 17f974edb839fb78e691376f7a021143474d8fcc

src: migrate sched_class for every task during upgrade and rollback
Only migrating sched_class for every task, we might think of sched_class
as a private data structure. And then we can change it whatever we want,
which help to imporve expressiveness of scheduler module.

Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

src/sched_rebuild.c
@@ -8,8 +8,48 @@
 
 extern void __orig_set_rq_offline(struct rq*);
 extern void __orig_set_rq_online(struct rq*);
-
 extern unsigned int process_id[];
+
+extern const struct sched_class __orig_stop_sched_class;
+extern const struct sched_class __orig_dl_sched_class;
+extern const struct sched_class __orig_rt_sched_class;
+extern const struct sched_class __orig_fair_sched_class;
+extern const struct sched_class __orig_idle_sched_class;
+
+static struct sched_class* class[][7] = {
+	{
+		&fair_sched_class,
+		&rt_sched_class,
+		&rt_sched_class,
+		&fair_sched_class,
+		NULL,
+		&idle_sched_class,
+		&dl_sched_class,
+		&stop_sched_class,
+	},
+	{
+		&__orig_fair_sched_class,
+		&__orig_rt_sched_class,
+		&__orig_rt_sched_class,
+		&__orig_fair_sched_class,
+		NULL,
+		&__orig_idle_sched_class,
+		&__orig_dl_sched_class,
+		&__orig_stop_sched_class,
+	}
+};
+
+static void switch_sched_class(struct task_struct *p, int mod)
+{
+	int policy = p->policy;
+	int idx = mod ? 0 : 1;
+
+	if (p == (task_rq(p)->stop))
+		policy = 7;
+
+	p->sched_class = class[idx][policy];
+	return;
+}
 
 void clear_sched_state(bool mod)
 {
@@ -58,6 +98,8 @@ void rebuild_sched_state(bool mod)
 	for_each_process_thread(g, p) {
 		if (rq != task_rq(p))
 			continue;
+
+		switch_sched_class(p, mod);
 
 		if (p == rq->stop)
 			continue;

commit c4b4734a8582a0df1f7fd545905a0a87a9bb7b82

src: sync the state of sched_class variables in place
To mark struct sched_class as private, commit 17f974e migrates
sched_class for every task. However, there are race windows during
task fork and task exit. In both cases, sched_class are inconsistent,
will lead to kernel panic. E.G:

copy_process():
  ->sched_fork()-> init to old sched_class
  ... (might sleep)

  ... plugsched fail to switch sched_class, for the new task is
      still not attacted to task_list.

  ->list_add_tail_rcu(&p->tasks, &init_task.tasks)
  ... using old sched_class!!!

do_exit():
  ->exit_notify()->release_task()->__exit_signal()->__unhash_process()
      ->list_del_rcu(&p->tasks)
  ... (maight sleep)

  ... plugsched fail to switch sched_class, for the dead task is
      detached from task_list now.

  ->do_task_dead()->__schedule()->dequeue_task()
  ... using new sched_class!!!

This patch fix it by sharing sched_class variables between vmlinux and
scheduler module, but re-init their state during scheduler upgrade and
rollback.

Fixes: 17f974e ("src: migrate sched_class for every task during
       upgrade and rollback")

Tested-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reported-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Analyzed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

src/main.c
@@ -181,8 +181,8 @@ static int __sync_sched_install(void *arg)
 	/* wait for all cpu to finish state rebuild */
 	atomic_cond_read_relaxed(&clear_finished, !VAL);
 
-	switch_sched_class(true);
 	if (is_first_process()) {
+		switch_sched_class(true);
 		JUMP_OPERATION(install);
 		disable_stack_protector();
 		sched_alloc_extrapad();
@@ -229,8 +229,8 @@ static int __sync_sched_restore(void *arg)
 	/* wait for all cpu to finish state rebuild */
 	atomic_cond_read_relaxed(&clear_finished, !VAL);
 
-	switch_sched_class(false);
 	if (is_first_process()) {
+		switch_sched_class(false);
 		JUMP_OPERATION(remove);
 		reset_balance_callback();
 		sched_free_extrapad();

src/sched_rebuild.c
@@ -10,67 +10,60 @@ extern void __orig_set_rq_offline(struct rq*);
 extern void __orig_set_rq_online(struct rq*);
 extern unsigned int process_id[];
 
-extern const struct sched_class __orig_stop_sched_class;
-extern const struct sched_class __orig_dl_sched_class;
-extern const struct sched_class __orig_rt_sched_class;
-extern const struct sched_class __orig_fair_sched_class;
-extern const struct sched_class __orig_idle_sched_class;
+extern struct sched_class __orig_stop_sched_class;
+extern struct sched_class __orig_dl_sched_class;
+extern struct sched_class __orig_rt_sched_class;
+extern struct sched_class __orig_fair_sched_class;
+extern struct sched_class __orig_idle_sched_class;
+extern struct sched_class shadow_stop_sched_class;
+extern struct sched_class shadow_dl_sched_class;
+extern struct sched_class shadow_rt_sched_class;
+extern struct sched_class shadow_fair_sched_class;
+extern struct sched_class shadow_idle_sched_class;
 
-#define SCHED_STOP 7
-
-static const struct sched_class* class[][SCHED_STOP+1] = {
-	{
-		&fair_sched_class,
-		&rt_sched_class,
-		&rt_sched_class,
-		&fair_sched_class,
-		NULL,
-		&idle_sched_class,
-		&dl_sched_class,
-		&stop_sched_class,
-	},
-	{
-		&__orig_fair_sched_class,
-		&__orig_rt_sched_class,
-		&__orig_rt_sched_class,
-		&__orig_fair_sched_class,
-		NULL,
-		&__orig_idle_sched_class,
-		&__orig_dl_sched_class,
-		&__orig_stop_sched_class,
-	}
+struct sched_class *orig_class[] = {
+	&__orig_stop_sched_class,
+	&__orig_dl_sched_class,
+	&__orig_rt_sched_class,
+	&__orig_fair_sched_class,
+	&__orig_idle_sched_class,
 };
+
+struct sched_class *mod_class[] = {
+	&shadow_stop_sched_class,
+	&shadow_dl_sched_class,
+	&shadow_rt_sched_class,
+	&shadow_fair_sched_class,
+	&shadow_idle_sched_class,
+};
+
+#define NR_SCHED_CLASS 5
+struct sched_class bak_class[NR_SCHED_CLASS];
+
+
+static inline void do_write_cr0(unsigned long val)
+{
+        asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
+}
 
 void switch_sched_class(bool mod)
 {
-	struct task_struct *g, *p;
-	int task_count = 0;
-	int nr_cpus = num_online_cpus();
-	int cpu = smp_processor_id();
-	int idx = mod ? 0 : 1;
+	int i;
+	int size = sizeof(struct sched_class);
+	unsigned long cr0 = read_cr0();
 
-	for_each_process_thread(g, p) {
-		if ((task_count % nr_cpus) == process_id[cpu]) {
-			int policy = p->policy;
+	do_write_cr0(cr0 & 0xfffeffff);
 
-			/* kernel doesn't set policy for stopper task */
-			if (p == task_rq(p)->stop)
-				policy = SCHED_STOP;
-			p->sched_class = class[idx][policy];
+	for (i = 0; i < NR_SCHED_CLASS; i++) {
+		if (mod) {
+			memcpy(&bak_class[i], orig_class[i], size);
+			memcpy(orig_class[i], mod_class[i], size);
+		} else {
+			memcpy(orig_class[i], &bak_class[i], size);
 		}
-		task_count++;
 	}
 
-	if (process_id[cpu])
-		return;
-
-	for_each_possible_cpu(cpu) {
-		/* kernel doesn't set policy for idle task */
-		p = idle_task(cpu);
-		p->sched_class = class[idx][SCHED_IDLE];
-	}
-
-	return;
+	do_write_cr0(cr0);
 }
 
 void clear_sched_state(bool mod)