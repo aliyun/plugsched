aliyun/plugsched

commit f0bc472d05a999f29fe8dc6364045a534e3dccac

cli:src: don't use lightweight stack check any more
lightweight stack check is hard to maintain, I hate it. Since
parallel stack check (just like kpatch and livepatch do, but
in parallel way) is fast enough, it's fine to use it as the only
stack safe check mechanism.

Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>
Reviewed-by: Erwei Deng <erwei@linux.alibaba.com>

src/main.c
@@ -9,11 +9,7 @@
 #include "helper.h"
 #include "mempool.h"
 #include "head_jump.h"
-#ifdef SCHEDMOD_STACK_SAFETY_LIGHT
-#include "stack_safe_light.h"
-#else
-#include "stack_safe_heavy.h"
-#endif
+#include "stack_check.h"
 
 #define RETRY_CNT 5
 #define MAX_CPU_NR		1024

cli.py
@@ -117,7 +117,7 @@ class Plugsched(object):
         logging.info('Patching kernel kbuild system')
         self.apply_patch('kbuild.patch')
 
-    def cmd_init(self, system_map, kernel_config='', kernel_customized=[], stack_check='light'):
+    def cmd_init(self, system_map, kernel_config='', kernel_customized=[]):
         if not os.path.exists(kernel_config):
             logging.fatal("Kernel config not specified")
         self.create_mod(kernel_config)
@@ -146,10 +146,6 @@ class Plugsched(object):
         self.apply_patch('module.patch')
         if 'builtin_springboard' not in kernel_customized:
             self.apply_patch('dynamic_springboard.patch')
-        if stack_check == 'light':
-            self.plugsched_sh.python('sched_boundary/sleepable.py', self.mod_path)
-            with open(os.path.join(self.mod_path, 'kernel/sched/mod/Makefile'), 'a') as f:
-                f.write('ccflags-y += "-DSCHEDMOD_STACK_SAFETY_LIGHT"')
         try:
             springboard = self.search_springboard('vmlinux')
 
@@ -213,16 +209,15 @@ class PlugschedCLI(object):
         depsh.make(jobs=j)
 
     def init(self, kernel_path, mod_path, kernel_debuginfo_path, j=1, kernel_config='',
-			system_map='', kernel_customized='', stack_check='light'):
+			system_map='', kernel_customized=''):
         """ Initialize a scheduler module for a specific kernel release and product
 
         :param j: Number of threads. "-j N" is okay while "-jN" is not allowed.
         :param kernel_config: Specify kernel_config to create scheduler module
         :param kernel_customized: builtin_springboard | task_life_hook
-        :param stack_check: light / heavy
         """
         self.plugsched = Plugsched(kernel_path, mod_path, threads=j, kernel_debuginfo_path=kernel_debuginfo_path)
-        self.plugsched.cmd_init(system_map, kernel_config, kernel_customized.split('|'), stack_check)
+        self.plugsched.cmd_init(system_map, kernel_config, kernel_customized.split('|'))
 
     def build(self, kernel_path, mod_path, kernel_devel_path, kernel_debuginfo_path, j=1):
         """ Build a scheduler module rpm package for a specific kernel release and product

sched_boundary/process.py
@@ -221,5 +221,3 @@ if __name__ == '__main__':
         dump(struct_properties, f, Dumper)
     with open('sched_boundary_extract.yaml', 'w') as f:
         dump(config, f, Dumper)
-    with open('sched_boundary_stack_safe.yaml', 'w') as f:
-        f.writelines(['{} {} {} {}\n'.format(*e) for e in edges])

sched_boundary/sleepable.py
@@ -1,142 +1,0 @@
-import os
-import re
-import sys
-import logging
-import coloredlogs
-from itertools import izip, groupby as _groupby
-from yaml import load, dump, CLoader as Loader, CDumper as Dumper
-from itertools import chain as _chain
-chain = _chain.from_iterable
-from multiprocessing import Pool, cpu_count
-
-coloredlogs.install(level='INFO')
-
-def mp_map(fn, lst):
-    pool = Pool(cpu_count())
-    res = pool.map(fn, lst)
-    pool.close()
-    pool.join()
-    res = chain(res)
-    return res
-
-def groupby(it, grouper, selector, reducer):
-    sorted_list = sorted(it, key=grouper)
-    return dict((k, reducer(map(selector, v))) for k, v in _groupby(sorted_list, grouper))
-
-if __name__ == '__main__':
-    logging.info('Running')
-    coloredlogs.install(level='INFO')
-    mod_path = sys.argv[1]
-
-with open(os.path.join(mod_path, 'sched_boundary_extract.yaml')) as f:
-    config = load(f, Loader)
-with open(os.path.join(mod_path, 'sched_boundary_stack_safe.yaml')) as f:
-    edges = groupby(map(str.split, f.readlines()),
-                    grouper=lambda (f, t, _, __):f,
-                    selector=lambda (f, t, carry, tail):(t, (eval(tail), eval(carry))),
-                    reducer=dict
-                   )
-
-# tail means A jump to B, not A calls B
-near_tail = [
-    ('scheduler_ipi', 'irq_exit'),
-    ('sched_feat_open', 'single_open'),
-    ('do_sched_yield', 'schedule'),
-]
-
-fixed = {
-    # Sure
-     'vprintk_emit'                                  :"it's an invalid path for scheduler"
-    ,'panic'                                         :"we don't care"
-    ,'mutex_lock(text_mutex)'                        :"as the name suggests"
-    ,'mutex_lock(jump_label_mutex)'                  :"as the name suggests"
-    ,'mutex_lock(cfs_constraints_mutex)'             :"as the name suggests"
-    ,'mutex_lock(shares_mutex)'                      :"as the name suggests"
-    ,'atomic_dec_and_mutex_lock(jump_label_mutex)'   :"as the name suggests"
-    ,'cpu_cgroup_css_alloc'                          :"we've held cgroup_mutx"
-    ,'partition_sched_domains'                       :"we checked sched_domains_mutex"
-    ,'ftrace_graph_init_idle_task'                   :"we checked ftrace_graph_active && !per_cpu(idle_ret_stack, cpu)"
-    ,'try_purge_vmap_area_lazy'                      :"we checked vmap_purge_lock"
-    ,'watchdog'                                      :"it's a false-positive caused by duplicate static method name"
-    ,'sched_cpu_deactivate'                          :"we've held cpu_maps_update_begin"
-    ,'sched_proc_update_handler'                     :"we checked kern_table's refcount"
-    ,'sysctl_blocked_averages'                       :"we checked kern_table's refcount"
-    ,'sysctl_tick_update_load'                       :"we checked kern_table's refcount"
-    ,'sysctl_schedstats'                             :"we checked kern_table's refcount"
-    ,"sysctl_numa_balancing"                         :"we checked kern_table's refcount"
-    ,"sched_rt_handler"	                             :"we checked kern_table's refcount"
-    ,"sched_rr_handler"	                             :"we checked kern_table's refcount"
-}
-
-sleep_leaf = [
-    'do_wait_for_common',
-    '__schedule',
-    # Not real leaf below, but to speed up
-    'kmalloc',
-    'kzalloc',
-    'kcalloc',
-]
-
-if len(sys.argv) > 1 and sys.argv[1] == '--no-fix':
-    fixed = {}
-    near_tail = []
-
-roots = set(config['function']['interface']) | set(config['function']['fn_ptr'])
-
-def find_sleep_path(root):
-    return dfs(root, {}, 0, True)
-
-all_sched_functions = set(config['function']['insider'])  | \
-                      set(config['function']['fn_ptr'])   | \
-                      set(config['function']['interface'])
-
-def dfs(u, visited, depth, all_tail):
-    if u in sleep_leaf:
-        if not all_tail:
-            return [[u]]
-        else:
-            return []
-
-    if depth > 20 or u not in edges or u in fixed:
-        return []
-
-    paths = []
-    for v, (tail, carry) in edges[u].iteritems():
-        #tail |= (u, v) in near_tail
-        tail = (u, v) in near_tail
-        if carry and '{}({})'.format(v, '.'.join(carry)) in fixed:
-            continue
-        u_in_sched = u in all_sched_functions
-        desc_paths = dfs(v, visited, depth + 1, all_tail & (not u_in_sched or tail))
-        paths += [[u] + path for path in desc_paths]
-    return paths
-
-logging.info('Start finding sleepable')
-all_paths = mp_map(find_sleep_path, roots - {'__schedule'} - set(fixed))
-
-fail = False
-
-for f, t in near_tail:
-    logging.info('%s calling %s nearly at the tail of %s', f, t, f)
-    logging.warn("WARNING: Better not modify any logic after %s calls %s, (include those functions called by %s)", f, t, f)
-
-for f, reason in fixed.items():
-    logging.info('Fixed stack safe issue: %s.', f)
-    if reason:
-        logging.info('   Because %s', reason)
-
-for path in all_paths:
-    out = 'UNSAFE STACK: ' + path[0]
-    for f, t in izip(path, path[1:]):
-        tail, carry = edges[f][t]
-        if tail or (f, t) in near_tail or f in all_sched_functions:
-            out += ' -> {}'.format(t)
-        else:
-            out += ' =$> {}'.format(t)
-    logging.error(out)
-    fail = True
-
-if fail is False:
-    logging.info('OK')
-else:
-    raise Exception("Stack check failed")

src/stack_safe_light.h
@@ -1,131 +1,0 @@
-#include <linux/mutex.h>
-#include <linux/mm_types.h>
-#include <linux/memcontrol.h>
-#include <linux/sched.h>
-#include <linux/wait.h>
-#include <linux/cpumask.h>
-#include <linux/sysctl.h>
-
-extern struct mutex jump_label_mutex;
-extern struct mutex text_mutex;
-extern struct mutex cfs_constraints_mutex;
-extern struct mutex shares_mutex;
-extern struct mutex sched_domains_mutex;
-extern struct mutex vmap_purge_lock;
-extern struct mutex cgroup_mutex;
-extern struct mutex cpuset_mutex;
-extern void cpu_maps_update_begin(void);
-extern void cpu_maps_update_done(void);
-
-DECLARE_PER_CPU(struct ftrace_ret_stack *, idle_ret_stack);
-
-struct mutex *mutex_check_list[] = {
-	&jump_label_mutex,
-	&text_mutex,
-	&cfs_constraints_mutex,
-	&shares_mutex,
-	&sched_domains_mutex,
-	&vmap_purge_lock,
-};
-
-extern int ftrace_graph_active;
-extern struct ctl_table_root sysctl_table_root;
-extern int namecmp(const char *name1, int len1, const char *name2, int len2);
-
-/* Called under sysctl_lock */
-static struct ctl_table *find_entry(struct ctl_table_header **phead,
-	struct ctl_dir *dir, const char *name, int namelen)
-{
-	struct ctl_table_header *head;
-	struct ctl_table *entry;
-	struct rb_node *node = dir->root.rb_node;
-
-	while (node)
-	{
-		struct ctl_node *ctl_node;
-		const char *procname;
-		int cmp;
-
-		ctl_node = rb_entry(node, struct ctl_node, node);
-		head = ctl_node->header;
-		entry = &head->ctl_table[ctl_node - head->node];
-		procname = entry->procname;
-
-		cmp = namecmp(name, namelen, procname, strlen(procname));
-		if (cmp < 0)
-			node = node->rb_left;
-		else if (cmp > 0)
-			node = node->rb_right;
-		else {
-			*phead = head;
-			return entry;
-		}
-	}
-	return NULL;
-}
-
-const char *sysctl_checker_list[] = {
-	"numa_balancing",
-	"sched_rt_period_us",
-	"sched_rt_runtime_us",
-	"sched_rr_timeslice_ms",
-	"sched_tick_update_load",
-	"sched_blocked_averages",
-	"sched_schedstats",
-	"sched_min_granularity_ns",
-	"sched_latency_ns",
-	"sched_wakeup_granularity_ns",
-	NULL
-};
-
-static int stack_check(bool install, int *error)
-{
-	int cpu;
-	struct mutex **mutex_checker;
-	struct ctl_table_header *kern_header, *header;
-	const char **sysctl_checker;
-	struct ctl_dir *kern_ctl_dir;
-
-	for (mutex_checker=mutex_check_list; *mutex_checker; mutex_checker++)
-		if (!list_empty(&(*mutex_checker)->wait_list))
-			goto fail;
-
-	for_each_online_cpu(cpu) {
-		if (ftrace_graph_active && !per_cpu(idle_ret_stack, cpu))
-			goto fail;
-	}
-
-	find_entry(&kern_header, &sysctl_table_root.default_set.dir, "kernel", 6);
-	kern_ctl_dir = container_of(kern_header, struct ctl_dir, header);
-
-	for (sysctl_checker=sysctl_checker_list; *sysctl_checker; sysctl_checker++) {
-		find_entry(&header, kern_ctl_dir, *sysctl_checker, strlen(*sysctl_checker));
-		if (header->used > 0)
-			goto fail;
-	}
-
-	*error = 0;
-	return 0;
-fail:
-	*error = -EAGAIN;
-	return 1;
-}
-
-static inline void stack_protect_open(void)
-{
-	/* Don't change the order of these locks! */
-	cpu_maps_update_begin();
-	mutex_lock(&cgroup_mutex);
-	mutex_lock(&cpuset_mutex);
-}
-
-static inline void stack_protect_close(void)
-{
-	/* Don't change the order of these locks! */
-	mutex_unlock(&cpuset_mutex);
-	mutex_unlock(&cgroup_mutex);
-	cpu_maps_update_done();
-}
-
-static inline void stack_check_insmod_init(void) {}
-static inline void stack_check_rmmod_init(void) {}

commit 406d75d568e95daa0c8cc3b019d358cf46ee975f

src: fix the bug that wrong num_online_cpus used when sync status
In function parallel_state_check_init() we get num_online_cpus and
assign it to some counters, which will be used to sync status in the
stop_machine stage: __sync_sched_install/restore(), but we don't
hold the lock of the cpumask during this process. If a cpu is
online/offline between parallel_state_check_init() and stop_machine(),
the counters we got before will be wrong, which will cause bad rip
or hard lockup.

To solve this problem, we lock the cpumask before
parallel_state_check_init() and unlock it after stop_machine().

Fixes: f0bc472d05a9("cli:src: don't use lightweight stack check any more")
Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

src/main.c
@@ -396,10 +396,12 @@ static int load_sched_routine(void)
 		return -ENOMEM;
 	}
 
+	cpu_maps_update_begin();
 	parallel_state_check_init();
 	process_id_init();
 
 	ret = sync_sched_mod(__sync_sched_install);
+	cpu_maps_update_done();
 	if (ret) {
 		sched_mempools_destroy();
 		module_put(THIS_MODULE);
@@ -429,10 +431,12 @@ static int unload_sched_routine(void)
 	printk("scheduler module is unloading\n");
 	main_start = ktime_get();
 
+	cpu_maps_update_begin();
 	parallel_state_check_init();
 	process_id_init();
 
 	ret = sync_sched_mod(__sync_sched_restore);
+	cpu_maps_update_done();
 	if (ret)
 		return ret;
 