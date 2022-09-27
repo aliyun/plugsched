aliyun/plugsched

commit c4b4734a8582a0df1f7fd545905a0a87a9bb7b82

src: sync the state of sched_class variables in place
To mark struct sched_class as private, commit 17f974e migrates
sched_class for every task. However, there are race windows during
task fork and task exit. In both cases, sched_class are inconsistent,
will lead to kernel panic. E.G:

copy_process():
  ->sched_fork()-> init to old sched_class
  ... (might sleep)

  ... plugsched fail to switch sched_class, for the new task is
      still not attacted to task_list.

  ->list_add_tail_rcu(&p->tasks, &init_task.tasks)
  ... using old sched_class!!!

do_exit():
  ->exit_notify()->release_task()->__exit_signal()->__unhash_process()
      ->list_del_rcu(&p->tasks)
  ... (maight sleep)

  ... plugsched fail to switch sched_class, for the dead task is
      detached from task_list now.

  ->do_task_dead()->__schedule()->dequeue_task()
  ... using new sched_class!!!

This patch fix it by sharing sched_class variables between vmlinux and
scheduler module, but re-init their state during scheduler upgrade and
rollback.

Fixes: 17f974e ("src: migrate sched_class for every task during
       upgrade and rollback")

Tested-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reported-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Analyzed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

src/main.c
@@ -181,8 +181,8 @@ static int __sync_sched_install(void *arg)
 	/* wait for all cpu to finish state rebuild */
 	atomic_cond_read_relaxed(&clear_finished, !VAL);
 
-	switch_sched_class(true);
 	if (is_first_process()) {
+		switch_sched_class(true);
 		JUMP_OPERATION(install);
 		disable_stack_protector();
 		sched_alloc_extrapad();
@@ -229,8 +229,8 @@ static int __sync_sched_restore(void *arg)
 	/* wait for all cpu to finish state rebuild */
 	atomic_cond_read_relaxed(&clear_finished, !VAL);
 
-	switch_sched_class(false);
 	if (is_first_process()) {
+		switch_sched_class(false);
 		JUMP_OPERATION(remove);
 		reset_balance_callback();
 		sched_free_extrapad();

src/sched_rebuild.c
@@ -10,67 +10,60 @@ extern void __orig_set_rq_offline(struct rq*);
 extern void __orig_set_rq_online(struct rq*);
 extern unsigned int process_id[];
 
-extern const struct sched_class __orig_stop_sched_class;
-extern const struct sched_class __orig_dl_sched_class;
-extern const struct sched_class __orig_rt_sched_class;
-extern const struct sched_class __orig_fair_sched_class;
-extern const struct sched_class __orig_idle_sched_class;
+extern struct sched_class __orig_stop_sched_class;
+extern struct sched_class __orig_dl_sched_class;
+extern struct sched_class __orig_rt_sched_class;
+extern struct sched_class __orig_fair_sched_class;
+extern struct sched_class __orig_idle_sched_class;
+extern struct sched_class shadow_stop_sched_class;
+extern struct sched_class shadow_dl_sched_class;
+extern struct sched_class shadow_rt_sched_class;
+extern struct sched_class shadow_fair_sched_class;
+extern struct sched_class shadow_idle_sched_class;
 
-#define SCHED_STOP 7
-
-static const struct sched_class* class[][SCHED_STOP+1] = {
-	{
-		&fair_sched_class,
-		&rt_sched_class,
-		&rt_sched_class,
-		&fair_sched_class,
-		NULL,
-		&idle_sched_class,
-		&dl_sched_class,
-		&stop_sched_class,
-	},
-	{
-		&__orig_fair_sched_class,
-		&__orig_rt_sched_class,
-		&__orig_rt_sched_class,
-		&__orig_fair_sched_class,
-		NULL,
-		&__orig_idle_sched_class,
-		&__orig_dl_sched_class,
-		&__orig_stop_sched_class,
-	}
+struct sched_class *orig_class[] = {
+	&__orig_stop_sched_class,
+	&__orig_dl_sched_class,
+	&__orig_rt_sched_class,
+	&__orig_fair_sched_class,
+	&__orig_idle_sched_class,
 };
+
+struct sched_class *mod_class[] = {
+	&shadow_stop_sched_class,
+	&shadow_dl_sched_class,
+	&shadow_rt_sched_class,
+	&shadow_fair_sched_class,
+	&shadow_idle_sched_class,
+};
+
+#define NR_SCHED_CLASS 5
+struct sched_class bak_class[NR_SCHED_CLASS];
+
+
+static inline void do_write_cr0(unsigned long val)
+{
+        asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
+}
 
 void switch_sched_class(bool mod)
 {
-	struct task_struct *g, *p;
-	int task_count = 0;
-	int nr_cpus = num_online_cpus();
-	int cpu = smp_processor_id();
-	int idx = mod ? 0 : 1;
+	int i;
+	int size = sizeof(struct sched_class);
+	unsigned long cr0 = read_cr0();
 
-	for_each_process_thread(g, p) {
-		if ((task_count % nr_cpus) == process_id[cpu]) {
-			int policy = p->policy;
+	do_write_cr0(cr0 & 0xfffeffff);
 
-			/* kernel doesn't set policy for stopper task */
-			if (p == task_rq(p)->stop)
-				policy = SCHED_STOP;
-			p->sched_class = class[idx][policy];
+	for (i = 0; i < NR_SCHED_CLASS; i++) {
+		if (mod) {
+			memcpy(&bak_class[i], orig_class[i], size);
+			memcpy(orig_class[i], mod_class[i], size);
+		} else {
+			memcpy(orig_class[i], &bak_class[i], size);
 		}
-		task_count++;
 	}
 
-	if (process_id[cpu])
-		return;
-
-	for_each_possible_cpu(cpu) {
-		/* kernel doesn't set policy for idle task */
-		p = idle_task(cpu);
-		p->sched_class = class[idx][SCHED_IDLE];
-	}
-
-	return;
+	do_write_cr0(cr0);
 }
 
 void clear_sched_state(bool mod)

commit 3154959e431d6555ac454ac48e0574c51ad22e6b

src: fix compile error on aarch64 platform
CR0 is specific to x86, can't work on aarch64. Using helper functions
to fix it.

Fixes: c4b4734 (src: sync the state of sched_class variables
       in place)

Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

src/head_jump.h
@@ -53,8 +53,8 @@ static unsigned long mod_func_size[NR_INTERFACE_FN];
  * 2) For functions set:
  *    1. Add the function to export_jump.h file
  *    2. Call jump_init_all() to init all functions data
- *    3. Use JUMP_OPERATION(install) macro to replace the functions set
- *    4. Use JUMP_OPERATION(remove) macro to restore the functions set
+ *    3. Call jump_install() to replace the functions set
+ *    4. Call jump_remove() to restore the functions set
  */
 
 #ifdef CONFIG_X86_64
@@ -93,20 +93,17 @@ extern void __orig___fentry__(void);
 #define JUMP_REMOVE_FUNC(func) 	\
 	memcpy((unsigned char *)__orig_##func, store_orig_##func, HEAD_LEN)
 
-static inline void do_write_cr0(unsigned long val)
+static inline void disable_write_protect(void)
 {
+	unsigned long val = read_cr0() & (~X86_CR0_WP);
 	asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
 }
 
-/* Must be used in stop machine context */
-#define JUMP_OPERATION(ops) do { 	\
-		unsigned long cr0;      \
-					\
-		cr0 = read_cr0();       \
-		do_write_cr0(cr0 & 0xfffeffff);    \
-		jump_##ops();		\
-		do_write_cr0(cr0);         \
-	} while(0)
+static inline void enable_write_protect(void)
+{
+	unsigned long val = read_cr0() | X86_CR0_WP;
+	asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
+}
 
 #else /* For ARM64 */
 #define DEFINE_JUMP_FUNC(func)				\
@@ -129,9 +126,8 @@ static inline void do_write_cr0(unsigned long val)
 #define JUMP_REMOVE_FUNC(func)  \
 	aarch64_insn_patch_text_nosync(__orig_##func, store_orig_##func)
 
-#define JUMP_OPERATION(ops) do {	\
-		jump_##ops();	\
-	} while(0)
+static inline void disable_write_protect(void) {}
+static inline void enable_write_protect(void) {}
 
 #endif /* CONFIG_X86_64 */
 

src/main.c
@@ -182,8 +182,10 @@ static int __sync_sched_install(void *arg)
 	atomic_cond_read_relaxed(&clear_finished, !VAL);
 
 	if (is_first_process()) {
+		disable_write_protect();
 		switch_sched_class(true);
-		JUMP_OPERATION(install);
+		jump_install();
+		enable_write_protect();
 		disable_stack_protector();
 		sched_alloc_extrapad();
 		reset_balance_callback();
@@ -230,8 +232,10 @@ static int __sync_sched_restore(void *arg)
 	atomic_cond_read_relaxed(&clear_finished, !VAL);
 
 	if (is_first_process()) {
+		disable_write_protect();
 		switch_sched_class(false);
-		JUMP_OPERATION(remove);
+		jump_remove();
+		enable_write_protect();
 		reset_balance_callback();
 		sched_free_extrapad();
 	}

src/sched_rebuild.c
@@ -41,18 +41,10 @@ struct sched_class *mod_class[] = {
 struct sched_class bak_class[NR_SCHED_CLASS];
 
 
-static inline void do_write_cr0(unsigned long val)
-{
-        asm volatile("mov %0,%%cr0": "+r" (val) : : "memory");
-}
-
 void switch_sched_class(bool mod)
 {
 	int i;
 	int size = sizeof(struct sched_class);
-	unsigned long cr0 = read_cr0();
-
-	do_write_cr0(cr0 & 0xfffeffff);
 
 	for (i = 0; i < NR_SCHED_CLASS; i++) {
 		if (mod) {
@@ -62,8 +54,6 @@ void switch_sched_class(bool mod)
 			memcpy(orig_class[i], &bak_class[i], size);
 		}
 	}
-
-	do_write_cr0(cr0);
 }
 
 void clear_sched_state(bool mod)