aliyun/plugsched

commit 259c48336f857612275ae685e33d6a647cf1886d

border: treat overriden weak symbols as outsiders
For function arch_asym_cpu_priority, arch/x86/kernel/itmt.c overrides
kernel/sched/fair.c. For function skb_copy_bits, net/core/skbuff.c
overrides kernel/bpf/core.c. And they're both weak functions.

When building vmlinux, such functions automatically gets overriden,
just as if they never exist. However when building the module, they
won't be overriden, because they stronger ones won't be available
until loading the module.

So this patch finds weak functions in the mod_path. And if they were
ever overriden by other strong functions, it marks them as outsiders
and their code gets deleted.

Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>

boundary/analyze.py
@@ -261,6 +261,7 @@ def func_class_arithmetics(fns):
     fns.inflect_cut = fns.border | fns.init | fns.sidecar
     fns.insider = inflect(fns.initial_insider, edges) - fns.init
     fns.sched_outsider = (fns.mod_fns - fns.insider - fns.border) | fns.cb_opt
+    fns.sched_outsider |= fns.fake_global & fns.mod_fns
     fns.outsider_opt = fns.sched_outsider - fns.in_vmlinux - fns.init
     fns.public_user = fns.fn - fns.insider - fns.border
     fns.tainted = (fns.border | fns.insider | fns.sidecar) & fns.in_vmlinux
@@ -329,6 +330,7 @@ if __name__ == '__main__':
         'sdcr_fns': set(),
         'interface': set(),
         'weak': set(),
+        'fake_global': set(),
     })
 
     edges = []
@@ -375,6 +377,9 @@ if __name__ == '__main__':
         if name != 'main' and len(fn_list) != 1 and fn_list[0][0] == fn_list[1][0]:
             print('warning: Can\'t tell which %s is linked in vmlinux!' % name)
         global_fn_dict[name] = fn_list[0][1]
+        for prio, file in fn_list[1:]:
+            if prio in (WEAK_ARCH, WEAK_NORM):
+                func_class.fake_global.add((name, file))
 
     # second pass: fix vague filename, calc callback and edge set
     for meta in metas:

commit c7d638c12fd857f912f1309fcddaa98c68d22938

border: exclude not-linked weak functions from tainted functions
If weak symbols within the boundary are not linked into vmlinux, then they
are classified as fake-global, because the real global symbols are the
corresponding strong symbols that reside in other source files.

fake-global functions are added to scheduler-outsiders after inflection,
so that their definitions can be removed. Also they should be removed from
the insider set too.

This patch influences only confliction checking against kpatch.

Fixes: 259c48336f85("border: treat overriden weak symbols as outsiders")
Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>

boundary/analyze.py
@@ -259,7 +259,7 @@ def func_class_arithmetics(fns):
 
     # Inflect outsider functions
     fns.inflect_cut = fns.border | fns.init | fns.sidecar
-    fns.insider = inflect(fns.initial_insider, edges) - fns.init
+    fns.insider = inflect(fns.initial_insider, edges) - fns.init - fns.fake_global
     fns.sched_outsider = (fns.mod_fns - fns.insider - fns.border) | fns.cb_opt
     fns.sched_outsider |= fns.fake_global & fns.mod_fns
     fns.outsider_opt = fns.sched_outsider - fns.in_vmlinux - fns.init