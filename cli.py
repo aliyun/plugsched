aliyun/plugsched

commit e4bd82b1c90abdd270c70ef1e5158e560db5fe68

border: integrate sidecar files into scheduler module automatically
Adding sidecar files into the Makefile of scheduler module, then
plugsched can find the text of sidecar functions.

Signed-off-by: Shanpei Chen <shanpeic@linux.alibaba.com>

cli.py
@@ -92,6 +92,7 @@ class Plugsched(object):
         self.mod_hdrs  = [f for f in self.mod_files if f.endswith('.h')]
         self.sdcr      = [] if self.config['sidecar'] is None else self.config['sidecar']
         self.sdcr_srcs = [f[1] for f in self.sdcr]
+        self.sdcr_objs = [os.path.basename(f).replace('.c', '.o') for f in self.sdcr_srcs]
         self.mod_objs  = [f+'.extract' for f in self.mod_files + self.sdcr_srcs]
 
     def get_kernel_version(self, makefile):
@@ -220,6 +221,7 @@ class Plugsched(object):
                             '--define', '%%_kerneldir %s' % os.path.realpath(self.work_dir),
                             '--define', '%%_tmpdir %s' % self.tmp_dir,
                             '--define', '%%_modpath %s' % self.mod_path,
+                            '--define', '%%_sdcrobjs %s' % ' '.join(self.sdcr_objs) + '""',
                             '--define', '%%KVER %s' % self.KVER,
                             '--define', '%%KREL %s' % self.KREL,
                             '--define', '%%threads %d' % self.threads,

module-contrib/scheduler.spec
@@ -34,7 +34,8 @@ rm -f %{_sourcedir}/scheduler.spec
 %build
 # Build sched_mod
 make KBUILD_MODPOST_WARN=1 plugsched_tmpdir=%{_tmpdir} plugsched_modpath=%{_modpath} \
-	-C %{_kerneldir} -f %{_tmpdir}/Makefile.plugsched plugsched -j %{threads}
+	sidecar_objs=%{_sdcrobjs} -C %{_kerneldir} -f %{_tmpdir}/Makefile.plugsched \
+	plugsched -j %{threads}
 
 # Build symbol resolve tool
 make -C %{_tmpdir}/symbol_resolve

src/Makefile
@@ -38,7 +38,7 @@ $(obj)/%.o: $(src)/%.c GET_STACK_SIZE FORCE
 	$(call if_changed_rule,cc_o_c)
 	$(OBJCOPY) $(OBJCOPYFLAGS) $(OBJCOPYFLAGS_$(@F)) $@
 
-scheduler-objs := $(objs-y) main.o sched_rebuild.o
+scheduler-objs := $(objs-y) $(sidecar_objs) main.o sched_rebuild.o
 
 ldflags-y += -T $(plugsched_modpath)/scheduler.lds
 ccflags-n += -DSCHEDMOD_MEMPOOL

commit 9ca6bcdd8cac4b88693ef1bf4d9dc3c55a21009e

cli: fix bug when building with multiple sidecar files
When defining _sdcrobjs, the quotes are in wrong position. So when
there are more than one sidecar files, all files except the first one
will be treated as params (i.e., build target) for Makefile, and cause
errors.

Fixes: e4bd82b1c90a ("border: integrate sidecar files into scheduler module automatically")
Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

cli.py
@@ -235,7 +235,7 @@ class Plugsched(object):
                             '--define', '%%_kerneldir %s' % os.path.realpath(self.work_dir),
                             '--define', '%%_tmpdir %s' % self.tmp_dir,
                             '--define', '%%_modpath %s' % self.mod_path,
-                            '--define', '%%_sdcrobjs %s' % ' '.join(self.sdcr_objs) + '""',
+                            '--define', '%%_sdcrobjs "%s"' % ' '.join(self.sdcr_objs),
                             '--define', '%%KVER %s' % self.KVER,
                             '--define', '%%KREL %s' % self.KREL,
                             '--define', '%%threads %d' % self.threads,